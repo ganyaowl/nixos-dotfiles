a73d631f7c248d1607d0175e7c186a4a6f6b1007
Ganijon Tashmatov <nephilim0363@gmail.com>
2025-12-13 22:02:25 +0500

    return gnome...

--- hosts/ganyaowl/home/configuration.nix
+++ hosts/ganyaowl/home/configuration.nix
@@ -39,7 +39,7 @@
   services.xserver.enable = true;
 
   services.displayManager.gdm.enable = true;
-  # services.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   services.xserver.xkb = {
     layout = "us,ru";
