a0b37e8ebd128a00585b9cbac735f235e5d3e098
Ganijon Tashmatov <nephilim0363@gmail.com>
2026-01-03 20:41:56 +0500

    add steam + proton

--- hosts/ganyaowl/home/configuration.nix
+++ hosts/ganyaowl/home/configuration.nix
@@ -92,6 +92,15 @@
     seahorse.enable = true;
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
